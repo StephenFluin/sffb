aef45e79603e51b2a2f566d06a8528f60f14663b
Stephen Fluin <stephen.fluin@gmail.com>
2018-01-10 17:00:41 -0800

    Create a library compatible with ng-packagr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SFFBModule } from './sffb/sffb.module';
 
 import { AppComponent } from './app.component';
-
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        SFFBModule,
+        AngularFireModule.initializeApp({
+            apiKey: 'AIzaSyAJawulOMYRp0eXjMHLqiffzuS9tToCfAI',
+            authDomain: 'fluindotio-website-93127.firebaseapp.com',
+            databaseURL: 'https://fluindotio-website-93127.firebaseio.com',
+            projectId: 'fluindotio-website-93127',
+            storageBucket: 'fluindotio-website-93127.appspot.com',
+            messagingSenderId: '1051668521425'
+          }),
+        AngularFireDatabaseModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
